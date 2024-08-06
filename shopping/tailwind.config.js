e16afe748d322cc8acfe7e1df69dd90f5ee2ace8
Pradip Mondal <pradipmondal629548@gmail.com>
2024-08-06 23:02:02 +0530

    delete

---
+++ shopping/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}', // Adjust this path based on your project structure
+    './src/**/*.html',       // HTML files in src directory
+    './src/**/*.ts'  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
